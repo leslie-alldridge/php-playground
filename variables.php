0397ff2583cd6c0f780069a7ac63600a25b06f3e
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-02-20 06:32:41 +1300

    init
    init

---
+++ variables.php
@@ -0,0 +1,33 @@
+<?php
+    // Single line comment
+    # Single line comment
+    /*
+        Multi
+        Line
+    */
+
+    #Variables
+    /*
+        -Prefix with $
+        -Start with letter or underscore
+        -Only letters numbers and underscores
+        -Case sensitive
+    */
+
+    #Data Types
+    /*
+        -String
+        -Integers
+        -Float
+        -Boolean
+        -Null
+        -Array
+        -Object
+    */
+
+    $output = 'Hello World';
+    $num1 = 4;
+    $float1 = 4.4;
+    $bool1 = true;
+    echo $output;
+?>
