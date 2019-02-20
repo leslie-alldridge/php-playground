f972640b52ea7276a0c5cc31ceae3a96560834a3
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-02-21 06:15:46 +1300

    md arrays
    md arrays

--- arrays.php
+++ arrays.php
@@ -21,4 +21,22 @@
     //print array
     //print_r($cars);
     //var_dump($cars); <- also shows data types
+
+    //Associative Arrays
+    $people = array('Brad' => 22, 'Leslie' => 35);
+    $ids = [22 => 'Brad', 35 => 'Leslie'];
+    echo $people['Leslie'];
+
+    $people['Jill'] = 42;
+
+    print_r($people);
+    
+    //Multi Dimensional
+    $cars = array(
+        array('Honda', 20, 10),
+        array('Toyota', 20, 2),
+        array('Ford', 22, 12),
+    );
+
+    echo $cars[1][2];
 ?>
